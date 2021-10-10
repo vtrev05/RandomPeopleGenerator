79e762eae056aafc438ffba625e5e7ad3b52c7ae
vtrev05 <lamary@gmail.comvtrev05@gmail.com>
2021-10-10 21:37:29 +0200

    done without styles

---
+++ main.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const searchButton = document.querySelector('.search')
+
+const getInfo = async () => {
+  const result = await fetch("https://api.generadordni.es/v2/profiles/person?results=1")
+  const data = await result.json()
+  const userInfo = data.map((element) => ({
+    name: element.fullname,
+    gender: element.gender,
+    birthDay: element.birthdate,
+    province: element.province,
+    nif: element.nif,
+    number: element.phonenumber
+  }));
+  //FULLNAME
+  document.querySelector('.fullname').innerHTML = `<h4 class="result">${userInfo[0].name}</h4>`
+  //GENDER
+  document.querySelector('.gender').innerHTML = `<h4 class="result">${userInfo[0].gender}</h4>`
+  //BIRTH DATE
+  document.querySelector('.birth').innerHTML = `<h4 class="result">${userInfo[0].birthDay}</h4>`
+  //PROVINCE
+  document.querySelector('.province').innerHTML = `<h4 class="result">${userInfo[0].province}</h4>`
+  //NIF
+  document.querySelector('.nif').innerHTML = `<h4 class="result">${userInfo[0].nif}</h4>`
+  //PHONE NUMBER
+  document.querySelector('.phoneNumber').innerHTML = `<h4 class="result">${userInfo[0].number}</h4>`
+}
+
+searchButton.addEventListener("click", getInfo); 
